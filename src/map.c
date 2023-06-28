85f66043bed529f9462a0b3b443e171dd2bf0a14
ZwingliCaleb <calebzwingli@gmail.com>
2023-06-28 21:14:56 +0000

    add map creation file

---
+++ src/map.c
@@ -0,0 +1,91 @@
+#include "../headers/header.h"
+
+static const int  map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
+	{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+	{6, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 6},
+	{6, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 6, 0, 0, 0, 6, 0, 0, 0, 6},
+	{6, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 6, 0, 7, 7, 0, 0, 0, 0, 6},
+	{6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 7, 0, 6},
+	{6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 6},
+	{6, 0, 0, 0, 0, 0, 7, 7, 7, 0, 0, 1, 0, 0, 0, 0, 7, 7, 0, 6},
+	{6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6},
+	{6, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 1, 0, 6},
+	{6, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 1, 0, 6},
+	{6, 0, 0, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6},
+	{6, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6},
+	{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6}
+};
+
+/**
+* DetectCollision - function to ensure that no collision takes place
+* @x: x coordinate.
+* @y: y coordinate.
+* Return: true if  a collision takes place otherwise false
+*/
+
+bool DetectCollision(float x, float y)
+{
+	int mapGridX, mapGridY;
+
+	if (x < 0 || x >= MAP_NUM_COLS * TILE_SIZE ||
+			y < 0 || y >= MAP_NUM_ROWS * TILE_SIZE)
+		return (true);
+	mapGridX = floor(x / TILE_SIZE);
+	mapGridY = floor(y / TILE_SIZE);
+	return (map[mapGridY][mapGridX] != 0);
+}
+
+/**
+* isInsideMap - keep in check operation within the map confines
+* @x: x coordinate
+* @y: y coordinate
+* @Return: true if within map and false if not within map
+*/
+
+bool isInsideMap(float x, float y)
+{
+	return (x >= 0 && x <= MAP_NUM_COLS * TILE_SIZE &&
+				y >= 0 && y <= MAP_NUM_ROWS * TILE_SIZE);
+}
+
+/**
+* getMapValue - func to check continuity with the map
+* @row: row to check in map
+* @column: column to check in map
+* @Return: position value in the map
+*/
+
+int getMapValue(int row, int col)
+{
+
+	return (map[row][col]);
+
+}
+
+/**
+* renderMap - func to render map
+*/
+
+void renderMap(void)
+{
+	int i, j, tileX, tileY;
+	color_t tileColor;
+
+	for (i = 0; i < MAP_NUM_ROWS; i++)
+	{
+		for (j = 0; j < MAP_NUM_COLS; j++)
+		{
+			tileX = j * TILE_SIZE;
+			tileY = i * TILE_SIZE;
+			tileColor = map[i][j] != 0 ? 0xFFFFFFFF : 0X00000000;
+
+			drawRect(
+				tileX * MINIMAP_SCALE_FACTOR,
+				tileY * MINIMAP_SCALE_FACTOR,
+				TILE_SIZE * MINIMAP_SCALE_FACTOR,
+				TILE_SIZE * MINIMAP_SCALE_FACTOR,
+				tileColor
+			);
+		}
+	}
+}
